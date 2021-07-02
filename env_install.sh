82a8c556c0249c35fb8cd05a75a1a28094586edb
israafeel <niqiesehe@qq.com>
2021-07-03 00:57:13 +0800

    adjust install sh

--- env_install.sh
+++ env_install.sh
@@ -1,30 +1,62 @@
 ##!/bin/bash
 
+finish(){
+    cp ./vimrc ~/.vimrc
+    exit 0
+}
+
 if [ -f "/etc/redhat-release" ]; then
     YUM='yum'
+    RELAY='python-devel python3-devel ruby-devel lua-devel libX11-devel gtk-develi gtk2-devel gtk3-devel ncurses-devel'
 elif [ -f "/etc/lsb-release" ]; then
     YUM='apt-get'
+    RELAY='vim'
 else
     echo 'I fuck up, man!!!'
     exit 1
 fi
-echo "package manager is $YUM"
+echo "Install start, package manager is $YUM"
 
-# 1. env install
-sudo $YUM install -y git
-# ruby, lua, perl, python2, python3
-sudo $YUM install -y python-devel python3-devel ruby-devel lua-devel libX11-devel gtk-develi gtk2-devel gtk3-devel ncurses-devel
+# 1. base env install
+sudo $YUM install -y git gcc
 
-# 2. deal vim 
+for sofeware in ruby lua5.3 perl python2 python3 ; do
+    echo "check $sofeware"
+    CHECK=`whereis $sofeware | grep -w "$sofeware: "`
+    echo "$sofeware path: $CHECK"
+    if [ -z "$CHECK" ];  then
+        echo "install $sofeware"
+	sudo $YUM install -y $sofeware
+    fi
+    if test $? -eq 0; then
+        echo "$sofeware had installed"
+    else
+	echo "$sofeware	install fail"
+	exit 1
+    fi
+done
+
+# 2. install plugin_manage
+if [ ! -d '~/.vim/bundle' ]; then
+   mkdir -p ~/.vim/bundle
+   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+# 3. install relay
+sudo $YUM install -y $RELAY
+if [ -f "/etc/lsb-release" ]; then
+    finish
+fi
+
+# 3. source install vim 
 if [ ! -d "./vim" ]; then
     git clone https://github.com/vim/vim.git
 fi
+cd vim
 
 # fetch env
-PY_CONFIG_DIR
+PY_CONFIG_DIR=`whereis python3 | xargs -n 1 |  grep "lib" | head -n 1`
 
-cd vim
-# pay attention here check directory correct
 sudo ./configure --with-features=huge \
      --enable-multibyte \
      --enable-rubyinterp \
@@ -41,18 +73,11 @@ sudo ./configure --with-features=huge \
 cd src
 make
 make install
-
 cd ../../
 
-# 3. deal plugin_manage
-if [ ! -d "~/.vim/bundle" ]; then
-   mkdir -p ~/.vim/bundle
-   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-
 # 4. check c++ env need set?
-read -p "install c++? (y/n)" is_install_c++
-case is_install_c++ in
+read -p "install c++? (y/n)" is_install_c
+case $is_install_c in
     [yY])
     # 4.1 make the utilsnips ruler
         mkdir -p ~/.vim/bundle/ultisnips/mysnippets/
@@ -73,3 +98,4 @@ case is_install_c++ in
 	;;
 esac
 
+finsh
